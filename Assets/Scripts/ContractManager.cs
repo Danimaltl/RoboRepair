89ebc234d5534012b91224839ca888cb3f648e25
Dan Cochran <danimaltl@gmail.com>
2020-02-01 22:46:56 -0700

    Added Aaron's script

---
+++ Assets/Scripts/ContractManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContractManager : MonoBehaviour
+{
+    Queue<Contract> contractQueue;
+    int nextID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contractQueue = new Queue<Contract>();
+        nextID = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            float min, max;
+            min = Random.Range(300, 1000);
+            max = Random.Range(min, min * 5);
+            Contract newContract = new Contract(nextID, min, max);
+            nextID++;
+            contractQueue.Enqueue(newContract);
+        }
+
+        if(Input.GetKeyDown("t"))
+        {
+            foreach (Contract contract in contractQueue)
+            {
+                Debug.Log("ID: " + contract.ID);
+                Debug.Log("Min val: " + contract.minVal);
+                Debug.Log("Max val: " + contract.maxVal);
+                Debug.Log("===========================");
+            }
+        }
+    }
+
+
+    internal class Contract
+    {
+        public int ID { get; }
+        public float minVal, maxVal;
+        public Robot cRobot;
+
+        internal Contract(int ID, float minVal, float maxVal)
+        {
+            this.ID = ID;
+            this.minVal = minVal;
+            this.maxVal = maxVal;
+            cRobot = new Robot(this.ID, this.minVal);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/RobotManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Robot
+{
+    public int ID;
+    private List<Limb> limbList;
+
+    public float value;
+
+    public string type;
+
+    public Robot(int ID, float minVal)
+    {
+        this.ID = ID;
+        CreateRobot(minVal);
+    }
+
+    private void CreateRobot(float minVal)
+    {
+        limbList = new List<Limb>();
+        int i = 0;
+        Debug.Log("MinVal: " + minVal);
+        while(minVal > value)
+        {
+            Limb newLimb = new Limb(i);
+            value += newLimb.value;
+            limbList.Add(newLimb);
+            i++;
+        }
+
+    }
+
+
+
+    public class Limb
+    {
+        public int ID;
+        public int quality;
+        public float value;
+
+        GameObject limbObject;
+
+            
+
+        public Limb(int type)
+        {
+
+            switch(type)
+            {
+                case 0: CreateTorso();
+                    break;
+                case 1: CreateHead();
+                    break;
+                default: CreateLimb();
+                    break;
+            }
+        }
+
+        private void CreateLimb()
+        {
+            var dLimb = (GameObject)Resources.Load("dLimb", typeof(GameObject));
+            quality = Random.Range(0, 100);
+            if (quality != 0)
+                value = Random.Range(100, 1000);
+            else
+                value = 0;
+
+            limbObject = Object.Instantiate(dLimb);
+        }
+
+        private void CreateTorso()
+        {
+            quality = 100;
+            value = 100;
+            limbObject = Object.Instantiate(Resources.Load("dTorso", typeof(GameObject))) as GameObject;
+        }
+
+        private void CreateHead()
+        {
+            var dHead = (GameObject)Resources.Load("dHead", typeof(GameObject));
+            quality = 100;
+            value = 100;
+            limbObject = Object.Instantiate(dHead);
+        }
+    }
+}
